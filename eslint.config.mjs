46b555841df7897985d412cf558c967de5ce5043
Dallas Whitten <dall.whitt@gmail.com>
2026-09-14 00:20:47 -0500

    Add ESLint 9 workspace configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/coverage/**",
+      "apps/desktop/dist-electron/**",
+      "apps/desktop/release/**",
+    ],
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+    },
+  },
+];
